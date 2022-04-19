0d2b9053b921d9227b8e7765b8a24f42f43ed1d2
Ssekuwanda <cytixdoug@gmail.com>
2022-04-19 23:09:20 +0300

    editted game controller

--- lib/game_controller.dart
+++ lib/game_controller.dart
@@ -15,22 +15,35 @@ List shuffle(List items) {
 }
 
 class GameController extends GetxController {
+  // The  pile of cards not yet played
   late RxList deck;
+  // The android /AI
   late RxList comp;
+  // Normal human playing the game
   late RxList human;
+  // A scene contains what has been played
   late RxList scene;
 
+  bool _humanTurn = true;
+  bool get humanTurn => _humanTurn;
+
+  // Starting the game
   @override
   onInit() {
     super.onInit();
+    // On start shuffle the game cards
     deck = shuffle(standardFiftyTwoCardDeck()).obs;
+    // Serve 7 cards to the players
     comp = deck.sublist(0, 7).obs;
     human = deck.sublist(8, 15).obs;
+    // Initiate the scene to have zero cards
     scene = [].obs;
+    // first player has played the first card
     scene.add(comp[1]);
     comp.remove(comp[1]);
   }
 
+  // Out of cards? load more cards
   loadCard() {
     human.add(deck[0]);
     deck.remove(deck[0]);
